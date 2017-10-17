43c12b915eabbcedd0c9dcc933da79799785a69f
Riad Djabri <riad@MacBook-Pro-de-Riad.local>
2017-10-17 16:06:29 +0200

    new php template parts

---
+++ wp-content/themes/easy/html/header.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<!--[if IE 8 ]><html class="ie" lang="en"><![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!--><html lang="en"><!--<![endif]-->
+<head>
+	<!-- title -->
+	<meta charset="utf-8">
+	<title>Vincent - Personal Portfolio Template</title>
+	
+	<!-- description -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no, maximum-scale=1">
+	<meta name="author" content="Pixelshow">
+	<meta name="description" content="Personal Portfolio Template">
+	
+	<!-- favicon -->
+	<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/images/favicon.png" type="image/png">
+	<link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?php echo get_stylesheet_directory_uri(); ?>/images/apple-touch-icon-114x114-precomposed.png">
+	<link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?php echo get_stylesheet_directory_uri(); ?>/images/apple-touch-icon-72x72-precomposed.png">
+	<link rel="apple-touch-icon-precomposed" sizes="57x57" href="<?php echo get_stylesheet_directory_uri(); ?>/images/apple-touch-icon-57x57-precomposed.png">
+	
+	<!-- fonts -->
+	<link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+	<link href="https://fonts.googleapis.com/css?family=Raleway:900" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Monoton" rel="stylesheet">
+
+	<!-- styles -->
+	<link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_directory_uri(); ?>/css/bootstrap.min.css"/>
+	<link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_directory_uri(); ?>/css/magnific-popup.min.css"/>
+	<link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_directory_uri(); ?>/css/font-awesome.min.css"/>
+	<link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_directory_uri(); ?>/css/style.css"/>
+
+	 <!-- Javascript libraries -->
+	<script type="text/javascript" src="<?php echo get_stylesheet_directory_uri(); ?>/js/modernizr.js"></script>
+	<script type="text/javascript" src="<?php echo get_stylesheet_directory_uri(); ?>/js/jquery.min.js"></script>
+		
+	<!--[if IE]>
+		<script src="<?php echo get_stylesheet_directory_uri(); ?>/js/html5shiv.js"></script>
+	<![endif]-->
+</head>
